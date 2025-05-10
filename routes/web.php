6b32a2843f8f08597968fd9576f629375d1e6ca3
Emils <emils@gmail.com>
2025-05-10 08:30:58 +0300

    show list of books

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\bookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/books', [bookController::class, 'index']);
+
